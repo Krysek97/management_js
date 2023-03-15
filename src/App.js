04c3d8826dea06ab6583823404c0de03016f09b1
Natalia <natalia.krysaa@gmail.com>
2023-03-15 11:31:32 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/transitions/slide.css';
+import AddE from "./pages/employee/Add.js";
+import All from "./pages/employee/All.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div>Zarządzanie</div>
+      <div style={{display:'flex'}}>
+    <Menu menuButton={<MenuButton>Pracownicy</MenuButton>}>
+      <MenuItem href="/employee/add">Nowy pracownik</MenuItem>
+      <MenuItem href="/employee/all">Lista pracowników</MenuItem>
+    </Menu>    
+    <Menu menuButton={<MenuButton>Grafik</MenuButton>}>
+      <MenuItem href="" >Nowy grafik</MenuItem>
+    </Menu>    
+    <a href="/"><button>Strona główna</button></a>
+    <a href="/login"><button>Zaloguj</button></a>
+    <a href="/register"><button>Zarejestruj</button></a>
     </div>
+      { <Routes>
+        {/* <Route path="/" element={<Office/>}/>
+         <Route path="*" element={<Error/>}/> */}
+        <Route path="employee/add" element={<AddE/>}/>
+        <Route path="employee/all" element={<All/>}/>
+        {/* <Route path="client/:id" element={<Client/>}/>
+         <Route path="client/:id/visits/add" element={<AddV/>}/> */}
+        {/* <Route path="login" element={<Login/>}/>
+         <Route path="register" element={<Registration/>}/> */}
+      </Routes> }
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/employee/Add.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Add(){
+  return (
+    <div>
+      <p>This is the Add</p>
+    </div>
+  )
+}
+
+export default Add;
