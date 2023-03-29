7ed92b02e818a64ec1c99c0fc5c4c4fc6f323e51
Natalia <natalia.krysaa@gmail.com>
2023-03-29 14:30:51 +0200

    main page layout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Sites from './templates/Sites.js';
 import Navbar from './templates/Navbar.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.js';
+
 
 function App() {
   return (
